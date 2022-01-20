7c4014fd667ef9bb56eebe0dbb2cfd4813b8fa4e
Mati36 <lopezmatias36@gmail.com>
2022-01-20 20:53:58 -0300

    init

---
+++ Readme.txt
@@ -0,0 +1,21 @@
+Readme 
+
+Install expo
+    npm install --global expo-cli 
+
+Run project 
+    expo start
+    
+    Error Error: error:0308010C:digital envelope routines::unsupported
+        export NODE_OPTIONS="--no-warnings --openssl-legacy-provider"    
+
+Stop project 
+
+Install dependences 
+    npm i    
+
+Create services and database example Firebase    
+
+Create .env and set keys 
+
+Run project
--- app.config..js
+++ app.config..js
@@ -1,4 +1,7 @@
-{
+import 'dotenv/config';
+
+
+export default {
   "expo": {
     "name": "ChatApp",
     "slug": "ChatApp",
@@ -27,6 +30,14 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDERLD,
+      appId: process.env.APP_ID
     }
   }
 }
